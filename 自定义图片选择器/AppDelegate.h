69cc1c26d166bfeb8417ed28fe134d568c41aeb0
wangyajun <yajunst@163.com>
2016-04-08 10:19:32 +0800

    Initial Commit

---
+++ 自定义图片选择器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义图片选择器
+//
+//  Created by 王亚军 on 16/4/8.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
